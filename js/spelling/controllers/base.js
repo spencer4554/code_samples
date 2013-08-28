a023351b3b3d06eab99ac9d5f96c04ece605cd39
Paul Prior <pprior@singleplatform.com>
2013-08-28 04:46:56 -0400

    Removing unneeded complexity from base controller.

--- js/spelling/controllers/base.js
+++ js/spelling/controllers/base.js
@@ -2,18 +2,6 @@
  * Base Controller
  */
 module.exports = {
-    // generic callback
-    callback: function (req, res, next) {
-	return function (err, data) {
-	    if (err) {
-		return next(err);
-	    }
-
-	    req.data = data;
-	    next();
-	};
-    },
-
     errorHandler:  function (err, req, res, next) {
 	res.send({'code': 500, 'errorType': 'UNKNOWN', 'errorMessage': 'An unexpected error has occurred. Please try again later.'});
     },
@@ -23,9 +11,9 @@ module.exports = {
 	    res.setHeader('Content-Type', 'application/json');
 	    var response = {'code': 200, 'data': req.data};
 	} else {
-	    res.setHeader('Content-Type', 'application/xml');
-	    var response = {'code': 200};
+            return next('App does not yet support data formats other than json.');
 	}
-	res.send(response);
+	
+        res.send(response);
     }
 };
