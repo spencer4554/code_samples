d2df09995256291d69b16883bb6fb258b9c6d199
Paul Prior <pprior@singleplatform.com>
2013-08-28 04:46:10 -0400

    Adding spellcheck functionality.

--- js/spelling/controllers/spelling.js
+++ js/spelling/controllers/spelling.js
@@ -1,5 +1,20 @@
-var base = require('./base');
+var SpellCheck = require('spellcheck'),
+    delimeter = (process.platform === 'win32' ? '\\' : '/'),
+    basedir = __dirname + delimeter + ".." + delimeter + "dictionaries" + delimeter,
+    spell = new SpellCheck(basedir + 'en_US.aff', basedir + 'en_US.dic');
 
 exports.spellCheck = function(req, res, next) {
-    return base.callback(req, res, next)(null, {'results': [], count: 0});
+    spell.check(req.params.word, function(err, correct, suggestions) {
+        if (err) {
+            return next(err);
+        }
+        else {
+            if (typeof suggestions == 'undefined') {
+                suggestions = [];
+            }
+
+            req.data = {'suggestions': suggestions, 'correct': correct};
+            next();
+        }
+    });
 };
--- js/spelling/tests/test.js
+++ js/spelling/tests/test.js
@@ -4,22 +4,80 @@ var http = require('http');
 var request = require('supertest');
 var assert = require('assert');
 
+function call_done_after_n(n, done) {
+    var iterations = 0;
+    return function () {
+        iterations += 1;
+        if (iterations == n) {
+            done();
+        }
+    }
+}
+
 describe('Test Spelling', function() {
-    it('Test if a valid word will return the correct response', function(done) {
+    it('Test if valid words return correct', function(done) {
+        var test_words = ['test', 'happy', 'lucky'];
+        done = call_done_after_n(test_words.length, done);
+
+        test_words.forEach(function (word) {
+	    request(app)
+	        .get('/spellcheck/' + word)
+	        .set('Content-Type','application/json')
+	        .expect(200)
+                .end(function(err, res) {
+                    var result = res.body;
+                    assert.equal(200, result.code);
+                    assert.equal(true, result.data.correct);
+                    assert.equal(result.data.suggestions.length, 0);
+                    done();
+                });
+        });
+    });
+
+    it('Test if incorrect spelling returns correct false', function(done) {
+        
+        var test_words = ['tesf', 'happf', 'luckf'];
+        done = call_done_after_n(test_words.length, done);
+        
+        test_words.forEach(function (word) {
+	    request(app)
+	        .get('/spellcheck/' + word)
+	        .set('Content-Type','application/json')
+	        .expect(200)
+                .end(function(err, res) {
+                    var result = res.body;
+                    assert.equal(200, result.code);
+                    assert.equal(false, result.data.correct);
+                    done();
+                });
+        });
+    });
+
+    it('Test if blatantly incorrect word produces no suggestions', function(done) {
 	request(app)
-	    .get('/spellcheck/test')
+	    .get('/spellcheck/supercalifragilisticexpialadocious')
 	    .set('Content-Type','application/json')
 	    .expect(200)
             .end(function(err, res) {
                var result = res.body;
                assert.equal(200, result.code);
-               assert.equal(result.data.count, 1);
+               assert.equal(false, result.data.correct);
+               assert.equal(result.data.suggestions.length, 0);
                done();
             });
     });
 
-    it('Test spelling check returns ', function(done) {
-        done()
+    it('Test if non ascii characters throws an error', function(done) {
+	request(app)
+	    .get('/spellcheck/œŁ好')
+	    .set('Content-Type','application/json')
+	    .expect(200)
+            .end(function(err, res) {
+               var result = res.body;
+               assert.equal(200, result.code);
+               assert.equal(false, result.data.correct);
+               done();
+            });
     });
 });
 
