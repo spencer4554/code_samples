50c60058427956cea51006fec702a4c49d26e229
Paul Prior <pprior@singleplatform.com>
2013-08-28 12:46:28 -0400

    Fixing run and run_tests script.

--- js/spelling/run.sh
+++ js/spelling/run.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+if [ -x /usr/libexec/path_helper ]; then
+	eval `/usr/libexec/path_helper -s`
+fi
+
+echo `which node`
+
+source `which virtualenvwrapper.sh`
 workon spelling
 source ./spelling/bin/activate 
 node ./app.js
--- js/spelling/run_tests.sh
+++ js/spelling/run_tests.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+if [ -x /usr/libexec/path_helper ]; then
+	eval `/usr/libexec/path_helper -s`
+fi
+
+source `which virtualenvwrapper.sh`
+workon spelling
+source ./spelling/bin/activate 
+
 cd tests; ../node_modules/.bin/mocha --debug; cd ..
