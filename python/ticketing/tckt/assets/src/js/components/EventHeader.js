b41d3e3ea3093aa2e82322afe24afd071171946b
seinna <contactseinna@yahoo.com>
2016-06-27 15:35:45 -0400

    start of sass, some mobile, and branch update

--- python/ticketing/tckt/assets/src/js/components/EventHeader.js
+++ python/ticketing/tckt/assets/src/js/components/EventHeader.js
@@ -16,12 +16,12 @@ var EventHeader = React.createClass({
                 <p className="event-date-day">{ this.props.day }</p>
               </div>
             </div>
-            <div className="large-7 columns">
+            <div className="large-7 columns" id="event-details">
               <span className="event-presenter">{ this.props.presenter }</span>
               <h2 className="event-name">{ this.props.name }</h2>
               <span className="event-subtitle">{ this.props.subtitle }</span>
             </div>
-            <div className="large-4 columns">
+            <div className="large-4 columns" id="event-location">
               <Location {...this.props.location} />
               <div className="event-start">
                 {this.props.startText}
